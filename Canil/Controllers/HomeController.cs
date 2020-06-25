3fe2e2cfa9fec169ec7a4883efcd7915367519bb
Christian Diz <3424.christian.diz@eped.pt>
2020-06-25 20:07:04 +0100

    Add project files.

---
+++ Canil/Controllers/HomeController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Canil.Models;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+using System.Net;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+
+namespace Canil.Controllers
+{   
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult login()
+        {
+            return View();
+        }
+
+        public IActionResult caes()
+        {
+            return View();
+        }
+        public IActionResult adote()
+        {
+            return View();
+        }
+        public IActionResult doaçoes()
+        {
+            return View();
+        }
+        public IActionResult loja()
+        {
+            return View();
+        }
+        public IActionResult canil()
+        {
+            return View();
+        }
+        public IActionResult saude()
+        {
+            return View();
+        }
+        public IActionResult treino()
+        {
+            return View();
+        }
+        public IActionResult atividades()
+        {
+            return View();
+        }
+
+        //[Authorize]
+        public IActionResult adicionar_cao()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> adicionar_cao(IFormFile file)
+        {
+            await UploadFile(file);
+            TempData["msg"] = "File Uploaded";
+            return View();
+        }
+
+        public async Task<bool> UploadFile(IFormFile file)
+        {
+            string path = "";
+            bool iscopied = false;
+            try
+            {
+                if (file != null)
+                {
+                    if (file.Length > 0)
+                    {
+                        string filename = Guid.NewGuid() + Path.GetExtension(file.FileName);
+                        path = Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), "Upload"));
+                        using (var filestream = new FileStream(Path.Combine(path, filename), FileMode.Create))
+                        {
+                            await file.CopyToAsync(filestream);
+                        }
+                        iscopied = true;
+                    }
+                    else
+                    {
+                        iscopied = false;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return iscopied;
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+    }
+}
---
+++ Canil/wwwroot/js/site.js
@@ -0,0 +1,39 @@
+﻿
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');    
+    const navLinks = document.querySelectorAll('.nav-links li');
+    const fixTransitionBug = document.getElementById('transition1');
+    //toggle nav
+    burger.addEventListener('click', () => {
+        fixTransitionBug.classList.add('transition-nav');
+        nav.classList.toggle('nav-active');
+
+           //animate links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+                setTimeout(() => {
+                    fixTransitionBug.classList.remove('transition-nav');
+                }, 500);
+            }
+            else {
+                if (index <= 5) {
+                    link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`
+                }
+                else {
+                    link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 - 0.9}s`
+                }
+            }
+        });
+
+        //burger animation
+        burger.classList.toggle('toggle');
+    });
+}
+
+navSlide();
+
+//const app = () => {
+//    navSlide();
+//}
