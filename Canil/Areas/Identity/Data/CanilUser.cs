f16dacf63a17f0dccac3d59da3d3c84a1aef5b40
Christian Diz <3424.christian.diz@eped.pt>
2020-06-27 07:16:45 +0100

    Sistema login 1uase acabado

---
+++ Canil/Areas/Identity/Data/CanilUser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace Canil.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the CanilUser class
+    public class CanilUser : IdentityUser
+    {
+        [PersonalData]
+        [Column(TypeName = "varchar(100)")]
+        public string Username { get; set; }
+
+        [PersonalData]
+        [Column(TypeName = "varchar(100)")]
+        public string email { get; set; }
+
+        [PersonalData]
+        [Column(TypeName = "varchar(100)")]
+        public string password { get; set; }        
+    }
+}
---
+++ Canil/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Canil.Areas.Identity.Data;
+using Canil.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Canil.Areas.Identity.IdentityHostingStartup))]
+namespace Canil.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<CanilContext>(options =>
+                    options.UseMySql(
+                        context.Configuration.GetConnectionString("DefaultConnection")));
+
+                services.AddDefaultIdentity<CanilUser>(options => options.SignIn.RequireConfirmedAccount = false)//true for email confirmation
+                    .AddEntityFrameworkStores<CanilContext>();
+            });
+        }
+    }
+}
